a1fe29669c8536f1d4633a84987e7e40d5f3d02d
WillC33 <williamcookenet@pm.me>
2024-04-15 18:00:08 +0100

    removed chat widget for maintenance

--- src/app.jsx
+++ src/app.jsx
@@ -11,7 +11,6 @@ export const App = () => (
     <ColourProvider>
         <LocationProvider>
             <Main />
-            <ChatWidget />
         </LocationProvider>
     </ColourProvider>
 )
